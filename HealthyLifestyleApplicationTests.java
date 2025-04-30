cfe0aaf8b7977b49bb305294315959ef12f6a9e3
Ronaldovich163RUS <146722679+Ronaldovich163RUS@users.noreply.github.com>
2025-04-30 23:30:07 +0400

    Добавление класса SpringBootTest
    Добавление класса Spring Boot для проверки работоспособности программы.

---
+++ HealthyLifestyleApplicationTests.java
@@ -0,0 +1,15 @@
+import com.example.healthlifestyle.HealthyLifestyle;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+        classes = {HealthyLifestyle.class}
+)
+class HealthyLifestyleApplicationTests {
+    HealthyLifestyleApplicationTests() {
+    }
+
+    @Test
+    void contextLoads() {
+    }
+}
